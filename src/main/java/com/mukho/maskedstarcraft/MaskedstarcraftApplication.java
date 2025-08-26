2f9a03e96a9c1097e8b28026f763c1d3811e0815
Mukho <mukho@kakao.com>
2025-08-26 19:38:25 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/mukho/maskedstarcraft/MaskedstarcraftApplication.java
@@ -0,0 +1,13 @@
+package com.mukho.maskedstarcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskedstarcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaskedstarcraftApplication.class, args);
+	}
+
+}
